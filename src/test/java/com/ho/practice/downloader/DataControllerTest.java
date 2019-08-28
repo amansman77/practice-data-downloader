d90a7bc35a9598a88ebd1d0ba6f35319632bfeb7
unknown <amansman77@gmail.com>
2019-08-28 13:51:14 +0900

    테스트 코드 추가

--- src/test/java/com/ho/practice/downloader/DataControllerTest.java
+++ src/test/java/com/ho/practice/downloader/DataControllerTest.java
@@ -1,34 +1,59 @@
 package com.ho.practice.downloader;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.File;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.ho.practice.downloader.controller.DataController;
+import com.ho.practice.downloader.repository.RawdataMongoDBRepository;
 
 @RunWith(SpringRunner.class)
-@RestClientTest(DataController.class)
+@WebMvcTest(DataController.class)
 public class DataControllerTest {
 	
-	@Autowired
-    private MockRestServiceServer server;
+    @Autowired
+    private MockMvc mvc;
 	
-	private String dataApiUrl = "http://localhost:8080/data";
-			
+    @Value("${data-path}")
+	private String dataPath;
+    
+    @MockBean
+	RawdataMongoDBRepository rawdataMongoDBRepository;
+    
 	@Test
-    public void testGetFindOneFromRemote() throws Exception {
-        server.expect(requestTo(dataApiUrl+"/file"))
-        	.andExpect(method(HttpMethod.POST))
-            .andRespond(withSuccess("success", MediaType.APPLICATION_JSON));
+    public void testCreateDataFile() throws Exception {
+        //given
+		Float cycle = 1.0f;
+		Integer day = 1;
+		
+        //when
+        final ResultActions actions = mvc.perform(post("/data/init")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .param("cycle", cycle.toString())
+                .param("day", day.toString()))
+                .andDo(print());
+        
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(content().string("success"))
+                ;
+        assertThat(new File(dataPath+"/data_" + cycle + "_" + day + ".csv").exists()).isEqualTo(true);
     }
-	
+
 }
